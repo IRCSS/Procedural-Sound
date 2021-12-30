3e5dfe9f8e98da06cffd0f17d1f2b7ab9f3c1b95
IRCSS <sh.shahrabi@hotmail.com>
2021-12-30 11:59:24 +0000

    started implemeting a music sheet

--- Assets/Scripts/Maps/KeyIndeciesToFrequencies.cs
+++ Assets/Scripts/Maps/KeyIndeciesToFrequencies.cs
@@ -2,16 +2,16 @@
 
 
 public enum KeyNamesToIndicies
-{
-    C0, CSharp0, D0, DSharp0, E0, F0, FSharp0, G0, GSharp0, A0, ASharp0, B0, // sub contra
-    C1, CSharp1, D1, DSharp1, E1, F1, FSharp1, G1, GSharp1, A1, ASharp1, B1, // contra
-    C2, CSharp2, D2, DSharp2, E2, F2, FSharp2, G2, GSharp2, A2, ASharp2, B2, // great
-    C3, CSharp3, D3, DSharp3, E3, F3, FSharp3, G3, GSharp3, A3, ASharp3, B3, // small
-    C4, CSharp4, D4, DSharp4, E4, F4, FSharp4, G4, GSharp4, A4, ASharp4, B4, // one line
-    C5, CSharp5, D5, DSharp5, E5, F5, FSharp5, G5, GSharp5, A5, ASharp5, B5, // second line
-    C6, CSharp6, D6, DSharp6, E6, F6, FSharp6, G6, GSharp6, A6, ASharp6, B6, // third line
-    C7, CSharp7, D7, DSharp7, E7, F7, FSharp7, G7, GSharp7, A7, ASharp7, B7, // fourth line
-    C8, CSharp8, D8, DSharp8, E8, F8, FSharp8, G8, GSharp8, A8, ASharp8, B8  // fith line
+{    /*    +0    +1     +2   +3      +4  +5    +6     +7    +8    +9    +10    +11   */
+/*  0*/    C0, CSharp0, D0, DSharp0, E0, F0, FSharp0, G0, GSharp0, A0, ASharp0, B0, // sub contra
+/* 12*/    C1, CSharp1, D1, DSharp1, E1, F1, FSharp1, G1, GSharp1, A1, ASharp1, B1, // contra
+/* 24*/    C2, CSharp2, D2, DSharp2, E2, F2, FSharp2, G2, GSharp2, A2, ASharp2, B2, // great
+/* 36*/    C3, CSharp3, D3, DSharp3, E3, F3, FSharp3, G3, GSharp3, A3, ASharp3, B3, // small
+/* 48*/    C4, CSharp4, D4, DSharp4, E4, F4, FSharp4, G4, GSharp4, A4, ASharp4, B4, // one line
+/* 60*/    C5, CSharp5, D5, DSharp5, E5, F5, FSharp5, G5, GSharp5, A5, ASharp5, B5, // second line
+/* 72*/    C6, CSharp6, D6, DSharp6, E6, F6, FSharp6, G6, GSharp6, A6, ASharp6, B6, // third line
+/* 84*/    C7, CSharp7, D7, DSharp7, E7, F7, FSharp7, G7, GSharp7, A7, ASharp7, B7, // fourth line
+/* 96*/    C8, CSharp8, D8, DSharp8, E8, F8, FSharp8, G8, GSharp8, A8, ASharp8, B8  // fith line
 }
 
 public enum OctavesName
---
+++ Assets/Scripts/MusicSheet.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[System.Serializable]
+public class SheetNote
+{
+    public float               noteBegin;     // At which beat does the note begin in a measure. For example if the time signature is 4/4 and it is on the second beat, this will be 2
+    public float               noteDuration;  // Again in beat units.No time used anywhere in the sheet.
+    public int                 notePositionInKey; // This is the note position on the staff, for example in the treble G Major, the G is the 3. I did it like this instead of scientific pitch because it is easier and faster to input music sheets. Also it is easy to switch keys on the same notes and it automaticly takes care of the sharps
+}
+
+[System.Serializable]
+public class Measure // A measure is a unit including a series of beats, for example 4 beats
+{
+    public SheetNote[] notesInTheMeasure;
+}
+
+[System.Serializable]
+public class Staff
+{
+    public KeyScale  Key;
+    public Measure[] measuresInTheSheet;
+}
+
+public enum KeyScale
+{
+    GMajor
+}
+[CreateAssetMenu(fileName = "AMusicSheet", menuName = "ScriptableObjects/MusicSheet", order = 1)]
+public class MusicSheet : ScriptableObject {
+
+    // For translating a music sheet to this format, here are some helpers to help along. 
+    // You need to take the notes from the treble and bass cleff and turn them to the midi indice/ scientific pitch notation
+    // The middle C (C4) is at key 48. The middle C is one ledger below the first lien of the treble cleff and one ledger above the 
+    // last line of the bass cleff. 
+    // First line of the Bass Cleff is a G, in 2 octaves below the middle C, so G2, which is the key 31
+    // Last line of the Bass Cleff is an A, two notes under the middle C, so key 45, or A3
+    // The First Line of the treble cleff is an E, 2 notes above a middle C, so at 52, E4
+    // The last line of the treble cleff is a F, 2 octaves above the middle C, so F5, with key index of 65
+
+    public int beatsPerMeasure;
+    public float noteValueSingleBeat;
+
+    public Staff Treble;
+    public Staff Bass;
+
+}
